37737d623948e4c289b789c0a8e59007b5a99308
Horki <ivanzvonimirhorvat@gmail.com>
2020-03-10 13:35:54 +0100

    format: HH:MM:SS using sstream

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,16 @@
 #include "format.h"
 
+#include <iomanip>
+#include <sstream>
 #include <string>
 
-using std::string;
-
-// TODO: Complete this helper function
+// DONE: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds [[maybe_unused]]) { return string(); }
+std::string Format::ElapsedTime(long seconds) {
+  std::stringstream r;
+  r.fill('0');
+  r << std::setw(2) << (seconds / 3600) << ":" << std::setw(2) << (seconds / 60)
+    << ":" << std::setw(2) << (seconds % 60);
+  return r.str();
+}
