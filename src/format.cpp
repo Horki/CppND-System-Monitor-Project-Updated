bf9cf7d7d307496f082f6e55aa8a61266c07a277
Horki <ivanzvonimirhorvat@gmail.com>
2020-03-11 14:19:58 +0100

    format: fix minutes; set prefix 0

--- src/format.cpp
+++ src/format.cpp
@@ -9,8 +9,8 @@
 // OUTPUT: HH:MM:SS
 std::string Format::ElapsedTime(long seconds) {
   std::stringstream r;
-  // r.fill('0');
-  r << std::setw(2) << (seconds / 3600) << ":" << std::setw(2) << (seconds / 60)
-    << ":" << std::setw(2) << (seconds % 60);
+  r.fill('0');
+  r << std::setw(2) << (seconds / 3600) << ":" << std::setw(2)
+    << ((seconds / 60) % 60) << ":" << std::setw(2) << (seconds % 60);
   return r.str();
 }
